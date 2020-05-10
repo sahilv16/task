a5206e5a1c66ff47d2668c83fa72587a2a93ac1e
sahilv16 <vermasahil350@gmail.com>
2020-05-10 13:04:59 +0530

    Updates

---
+++ precache-manifest.2743aba517551bcea7af3b3ced6087ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "344249fd99539a4b498b",
+    "url": "/ecommerce-shopping-cart/static/css/main.18dc23eb.chunk.css"
+  },
+  {
+    "revision": "344249fd99539a4b498b",
+    "url": "/ecommerce-shopping-cart/static/js/main.ce94e6d9.chunk.js"
+  },
+  {
+    "revision": "a0e27bc38c15e076523a",
+    "url": "/ecommerce-shopping-cart/static/js/runtime~main.e9daa8c9.js"
+  },
+  {
+    "revision": "8748c926587665e6bdfe",
+    "url": "/ecommerce-shopping-cart/static/js/2.e554ec1a.chunk.js"
+  },
+  {
+    "revision": "f2a906c2b300f506cb4d52a6704965d7",
+    "url": "/ecommerce-shopping-cart/index.html"
+  }
+];
